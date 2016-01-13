740113e0b85ab1855b175f45480d0df51f6afc50
Alex Deckmyn <alex.deckmyn@gmail.com>
2016-01-13 20:11:37 +0100

    New world map without lakes, but separate lakes database added.

--- R/read.polygons.R
+++ R/read.polygons.R
@@ -1,6 +1,6 @@
 # transform a SpatialPolygons[DataFrame] into a list of polygons for map()
 # somewhat geared towards Natural Earth data (which has 'name' and 'iso_a2' columns)
-SpatialPolygons2map <- function(database, a2code=NULL, namefield=NULL){
+SpatialPolygons2map <- function(database, a2code=NULL, namefield="names"){
 #  if (is.character(database)) database <- readShapePoly(database)
   if(!inherits(database,"SpatialPolygons")) stop("database must be a SpatialPolygons[DataFrame] object.")
 
@@ -12,17 +12,10 @@ SpatialPolygons2map <- function(database, a2code=NULL, namefield=NULL){
         database <- database[a2 %in% a2code, ]
       } else warning("No column iso_a2 found.")
     }
-    if (is.null(namefield)) {
-# we use as default:
-      namefield <- "name"
-# but we don't enforce this
-      lforce <- FALSE
-    } else lforce <- TRUE
     namcol <- which(tolower(names(database)) == tolower(namefield))
     if (length(namcol) == 1) region.names <- as.character(database@data[[namcol]])
     else {
-      if (lforce) stop(paste0("database does not contain the field '",namefield,"'."))
-#      warning(paste0("database does not contain the field '",namefield,"'."))
+      warning(paste0("database does not contain the field '",namefield,"'."))
       region.names <- unlist(lapply(database@polygons, function(x) x@ID))
     }
   } else {
@@ -46,8 +39,8 @@ SpatialPolygons2map <- function(database, a2code=NULL, namefield=NULL){
 #  p2 <- lapply(p1, function(x) rbind(x,c(NA,NA)))
 #  p3 <- do.call(rbind,p2)
   result <- do.call(rbind, lapply(do.call(c,allpoly), 
-                                  function(x) rbind(x,c(NA,NA))))
-  list(x = head(result[,1],-1), y = head(result[,2],-1), names = gon.names,
+                                  function(x) rbind(c(NA,NA),x)))
+  list(x = result[-1,1], y = result[-1,2], names = gon.names,
        range = c(range(result[,1], na.rm = TRUE),range(result[,2], na.rm = TRUE)))
 }
 
