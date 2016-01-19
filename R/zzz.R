89758b97ed0b9206943031bb141aa44cdcc313da
Alex Deckmyn <alex.deckmyn@gmail.com>
2016-01-19 19:16:29 +0100

    cleanup man pages & README's

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(lib,pkg) {
   packageStartupMessage("\n",
 #      " ############################################################\n",
-      " # ATTENTION: maps v3.0 has an updated 'world' map.        #\n",
-      " # Many country borders and names have changed since 1990. #\n",
+      " # ATTENTION: maps v3.1 has an updated 'world' map.        #\n",
+      " # All lakes have been moved to the new 'lakes' database.  #\n",
       " # Type '?world' or 'news(package=\"maps\")'. See README_v3. #\n",
 #      " ############################################################\n")
       "\n")
