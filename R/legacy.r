7fd31e5831998bcdae9012fc1c33b7a987330a1a
Alex Deckmyn <alex.deckmyn@meteo.be>
2016-01-27 11:53:04 +0100

    add .Deprecated() to world.legacy()

--- R/legacy.r
+++ R/legacy.r
@@ -1,7 +1,7 @@
 # a temporary function to switch to the old legacy world map (1990)
 world.legacy <- function(wl=FALSE){
-  warning("The function 'world.legacy()' will be removed in v3.2.")
   if(wl) Sys.setenv("R_MAP_DATA_DIR_WORLD" =
                   paste(Sys.getenv("R_MAP_DATA_DIR"),"legacy_", sep="/"))
   else Sys.setenv("R_MAP_DATA_DIR_WORLD" = Sys.getenv("R_MAP_DATA_DIR"))
+  .Deprecated('map("legacy_world",...)')
 }
