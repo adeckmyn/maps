90898be5e62721d5ae49928d4fcc528914e8fec0
Alex Deckmyn <alex.deckmyn@meteo.be>
2017-06-21 12:41:25 +0200

    Start updating world.cities using geonames data

---
+++ data-raw/make.world.cities.R
@@ -0,0 +1,32 @@
+srcdata="http://download.geonames.org/export/dump/cities5000.zip"
+zipfile=basename(srcdata)
+infile="cities5000.txt"
+
+download.file(srcdata, zipfile)
+
+c1 <- read.table(unz(zipfile, infile), stringsAsFactor=FALSE, sep="\t", header=FALSE, quote="")
+c1 <- c1[,c(3,5,6,8,9,15,19)]
+names(c1) <- c("name","lat","lon","code","country","pop","date")
+
+### compare/validate
+c0 <- maps::world.cities
+
+# $code has 15 possible values:
+# [1] "PPLA"  "PPLC"  "PPL"   "PPLA2" "PPLA3" "PPLX"  "PPLA4" "PPLL"  "PPLG" 
+# [10] "PPLH"  "PPLQ"  "PPLS"  "PPLR"  "PPLF"  "PPLW" 
+# 
+c1$capital <- 0
+c1$capital[c1$code=="PPLC"] <- 1
+
+
+# compare by name, lat, lon, country...
+# if pop > 5000, it should be in both
+
+# ~22000 same names = ~50%
+t1 <- intersect(c1$name, c0$name)
+# ATTENTION: sometimes many cities with same name -> use lat/lon or country...
+# how check the rest? does it matter?
+world.cities.new <- c1
+save("world.cities.new", file="../data/world.cities.new.rda")
+write.table(c1,  file="./world.cities.new.tsv", row.names=FALSE, quote=TRUE, sep="\t")
+
