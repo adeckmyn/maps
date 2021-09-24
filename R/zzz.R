02d858d47d40be31eac202d5f85ca4f9892fc527
Alex Deckmyn <alex.deckmyn@gmail.com>
2021-09-24 21:13:49 +0200

    re-introduce R_MAP_DATA_DIR_WORLD

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,11 @@
 .onLoad <- function(lib, pkg) {
-  if (Sys.getenv("R_MAP_DATA_DIR") == "")
+  if (Sys.getenv("R_MAP_DATA_DIR") == "") {
     Sys.setenv("R_MAP_DATA_DIR" = paste(lib, pkg, "mapdata/", sep="/"))
+  }
+  ## for now, we keep the option of having a different world map
+  if (Sys.getenv("R_MAP_DATA_DIR_WORLD") == "") {
+    Sys.setenv("R_MAP_DATA_DIR_WORLD" = Sys.getenv("R_MAP_DATA_DIR"))
+  }
   library.dynam("maps", pkg, lib)
 }
 
--- data-raw/makedata-mapenv.R
+++ data-raw/makedata-mapenv.R
@@ -3,7 +3,8 @@
 MAPENV <- "R_MAP_DATA_DIR"
 OUTDIR <- "../data"
 
-datalist <- c("world", "world2", "lakes",
+#datalist <- c("world", "world2", "lakes",
+datalist <- c("lakes",
               "county", "state", "usa", "state", "state.vbm", "state.carto",
               "italy", "france", "nz")
 
@@ -12,4 +13,11 @@ for (dd in datalist) {
   assign(dname, MAPENV)
   save(list=dname, file=paste0(OUTDIR,"/",dname,".rda"))
 }
+# some (temporary?) exceptions:
+for (dd in c("world", "world2")) {
+  dname <- paste0(dd, "MapEnv")
+  assign(dname, "R_MAP_DATA_DIR_WORLD")
+  save(list=dname, file=paste0(OUTDIR,"/",dname,".rda"))
+}
+
 
