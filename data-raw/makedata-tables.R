fb7b67cf6f74b6f1210c10b56430b2d0f64e5310
Alex Deckmyn <alex.deckmyn@meteo.be>
2025-01-10 10:02:50 +0100

    Update man page for external formats

--- data-raw/makedata-tables.R
+++ data-raw/makedata-tables.R
@@ -12,13 +12,18 @@ rda2tsv <- function(dlist=datalist) {
   }
 }
 
-tsv2rda <-  function(dlist=datalist) {
+tsv2rda <-  function(dlist) {
+  # don't run this on all data sets at once! Some need special care.
   for (dname in dlist) {
     infile <- paste0("../data-raw/",dname,".tsv")
     outfile <- paste0("../data/",dname,".rda")
+    # in iso3166, there are no missing entries and "NA" is Namibia
+    # but in other tables, we must account for missing values
+    nas <- if (dname=="iso3166") "" else "NA"
 
+    # Some data sets are saved as a MATRIX, not a data.table
     zz <- read.table(infile, stringsAsFactors=FALSE,
-                     sep="\t", header=TRUE, na.strings="")
+                     sep="\t", header=TRUE, na.strings=nas)
     assign(dname, zz)
     save(list=dname, file=outfile)
   }
